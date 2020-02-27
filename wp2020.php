c852aebc9ac29400ef05b896945f06ad0ede6e81
SiteBuilderOne <anton@sitebuilderone.com>
2020-02-26 16:15:35 -0800

    Name chage

--- wp2020.php
+++ wp2020.php
@@ -1,8 +1,8 @@
 /**
- * Plugin Name:       Optimizer by SitebuilderOne
+ * Plugin Name:       * Optimizer by SitebuilderOne
  * Plugin URI:        https://github.com/sitebuilderone/wp2020/
  * Description:       Removes unecessary items from WordPress to make leaner and faster
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Anthony Lepki
  * License:           GNU General Public License v2
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
