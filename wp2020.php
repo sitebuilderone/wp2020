320bd09cc0f7f7acf8721a67d3c5dcf7f67b9a31
SiteBuilderOne <anton@sitebuilderone.com>
2020-02-26 16:07:11 -0800

    .1

--- wp2020.php
+++ wp2020.php
@@ -2,7 +2,7 @@
  * Plugin Name:       Optimizer by SitebuilderOne
  * Plugin URI:        https://github.com/sitebuilderone/wp2020/
  * Description:       Removes unecessary items from WordPress to make leaner and faster
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Anthony Lepki
  * License:           GNU General Public License v2
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
