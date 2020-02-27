ec24ebf8c457bc772c24448cbfc972a1b8aec0f3
SiteBuilderOne <anton@sitebuilderone.com>
2020-02-26 16:04:32 -0800

    New plugin

---
+++ wp2020.php
@@ -0,0 +1,12 @@
+/**
+ * Plugin Name:       Optimizer by SitebuilderOne
+ * Plugin URI:        https://github.com/sitebuilderone/wp2020/
+ * Description:       A plugin to automatically update GitHub, Bitbucket or GitLab hosted plugins and themes. It also allows for remote installation of plugins or themes into WordPress.
+ * Version:           1.0.0
+ * Author:            Anthony Lepki
+ * License:           GNU General Public License v2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path:       /languages
+ * Text Domain:       wp2020
+ * GitHub Plugin URI: https://github.com/sitebuilderone/wp2020/
+ */
